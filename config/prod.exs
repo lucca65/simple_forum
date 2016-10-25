1a3a4d7d2ee9599982b2ce3e5d6d0523bdab9835
Lucca <julienlucca@gmail.com>
2016-10-25 20:42:01 -0200

    getting ready for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,21 @@ use Mix.Config
 # which you typically run after static files are built.
 config :simple_forum, SimpleForum.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  # url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "edukforum.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+
+config :simple_forum, SimpleForum.Endpoint,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -55,7 +65,3 @@ config :logger, level: :info
 #
 #     config :simple_forum, SimpleForum.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
