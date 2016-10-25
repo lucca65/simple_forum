c46f275ff0490e2533965d79a58491d44f5d08eb
Lucca <julienlucca@gmail.com>
2016-10-25 21:05:18 -0200

    fixed configs

--- config/prod.exs
+++ config/prod.exs
@@ -13,21 +13,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :simple_forum, SimpleForum.Endpoint,
   http: [port: {:system, "PORT"}],
-  # url: [host: "example.com", port: 80],
   url: [scheme: "https", host: "edukforum.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
-
-# Do not print debug messages in production
-config :logger, level: :info
-
-
-config :simple_forum, SimpleForum.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 # ## SSL Support
 #
